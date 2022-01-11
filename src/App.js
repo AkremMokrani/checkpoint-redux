5fb981fbdc90a604fcfdc3af01206c79246c967c
AkremMokrani <mokraniakrem1920@gmail.com>
2022-01-11 21:43:01 +0100

    redux

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Task from './JS/Components/Task';
+import './App.css'
+import TaskList from './JS/Components/TaskList';
+//import EditTask from './JS/Components/EditTask';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <Task />
+    <TaskList/>
+   
+    </div>
+  );
+}
+
+export default App;
